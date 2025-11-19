607bdb7d6799b87998680c72ab9b76269185b0b7
LeyendaArlequins <161185259+LeyendaArlequins@users.noreply.github.com>
2025-11-19 12:12:04 -0600

    Update lg.js

--- lg.js
+++ lg.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const CHANNEL_ID = "1437151487141740637";
-
+const TOKEN = procces.env.TOKEN
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
